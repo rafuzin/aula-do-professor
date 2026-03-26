6a3ede07085a178f3220eaf2fd955e18534e7eb7
Rafuzin <Rafafmartins2009@gmail.com>
2026-03-26 15:57:42 -0300

    adcionado/vida

--- PALAVRA.js
+++ PALAVRA.js
@@ -2,6 +2,9 @@ let letErrada = [];
 let letCorreta = [];
 let exibicao = [];
 
+let vida = 6;
+
+
 function habilitarBTIncio(){
     const palav = document.getElementById("segredo").value;
 
@@ -25,13 +28,55 @@ function VerificaCaracter(){
     
     let palavraSec = document.getElementById("segredo").value.toUpperCase()
     let Letra = document.getElementById("tentativa").value.toUpperCase();
-
+    
+    if(letErrada.includes(Letra) || letCorreta.includes(Letra)){
+        alert("Letra ja digitada");
+        document.getElementById("tentativa").value = "";
+        return;
+    }
+    
     if(palavraSec.includes(Letra)){
+        letCorreta.push(Letra);
         for(let i=0; i<palavraSec.length; i++){
             if(palavraSec[i] === Letra){
                 exibicao[i] = Letra;
             }   
         }
         document.getElementById("painel").innerText = exibicao.join(' ');
+        
+        if(!exibicao.includes('_')){
+            alert("Voce ganhou o jogo");
+            document.getElementById("tentativa").value = "";
+        }
     }
+    
+    else{
+        document.getElementById("tentativa").value = "";
+        letErrada.push(Letra);
+        document.getElementById("Erradas").innerText = letErrada.join(', ');
+        
+        vida = vida - 1;
+
+        document.getElementById("vidaImg").src = imagens[vida];
+
+        if(vida === 0){
+            alert("Voce perdeu o jogo");
+
+            
+        }
     }
+
+
+    document.getElementById("tentativa").value = "";
+    }
+
+    let vidas = 6 [
+        "0 barras.png",
+        "1 barras.png",
+        "2 barras.png",
+        "3 barras.png",
+        "4 barras.png",
+        "5 barras.png",
+        "6 barras.png"
+    ]
+    
